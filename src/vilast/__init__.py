0dfee0a4275cf3586acd662e691d8d401013aeba
cukucu123 <cukucu12@snu.ac.kr>
2024-09-18 13:22:50 +0900

    refactring

---
+++ src/vilast/__init__.py
@@ -0,0 +1,10 @@
+# __init__.py for the vilast package
+
+# Metadata
+__version__ = "0.1.0"
+__author__ = "Your Name"
+
+# Importing modules for easier access
+from vilast.taxonomy import Taxonomy
+
+# You can also define any package-level constants here if needed
